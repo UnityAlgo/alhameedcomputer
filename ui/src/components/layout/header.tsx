8e1fca8c82b29d221dd4399065427df52a42413e
Fuzail Abbas <fuzailkazmi122@gmail.com>
2025-11-17 18:28:18 +0500

    -

--- ui/src/components/layout/header.tsx
+++ ui/src/components/layout/header.tsx
@@ -42,7 +42,7 @@ export const Header = () => {
           <div className="flex items-center gap-2 md:hidden">
             <button
               onClick={() => setIsActive(!isActive)}
-              className=""
+              className="mt-1.5"
             >
               <Menu className="h-7 w-7" />
             </button  >
